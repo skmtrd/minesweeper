0b702852828abfe40b82d6207f10d504d9579688
skmtrd <skmtgame23@gmail.com>
2024-05-08 17:46:26 +0900

    blibding cell design complite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const getRandomIntNumber = (min, max) => {
+  return [
+    Math.floor(Math.random() * (max - min + 1)) + min,
+    Math.floor(Math.random() * (max - min + 1)) + min,
+  ];
+};
+
 const Home = () => {
   const [bombMap, setBombMap] = useState([
+    [1, -1, -2, -3, -4, -5, -6, -7, -8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [frontBoard, setFrontBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,6 +31,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
   return (
     <div className={styles.container}>
       <div className={styles.baseDisplayStyle}>
@@ -24,7 +43,63 @@ const Home = () => {
             row.map((color, x) => (
               <div className={styles.cellStyle} key={`${x}-${y}`}>
                 <div className={styles.backCellStyle}>
-                  <div className={styles.imageStyle} />
+                  {color === 1 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-220px 0px` }}
+                    />
+                  )}
+                  {color === -1 && (
+                    <div className={styles.imageStyle} style={{ backgroundPosition: `0px 0px` }} />
+                  )}
+                  {color === -2 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-22px 0px` }}
+                    />
+                  )}
+                  {color === -3 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-44px 0px` }}
+                    />
+                  )}
+                  {color === -4 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-66px 0px` }}
+                    />
+                  )}
+                  {color === -5 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-88px 0px` }}
+                    />
+                  )}
+                  {color === -6 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-110px 0px` }}
+                    />
+                  )}
+                  {color === -7 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-132px 0px` }}
+                    />
+                  )}
+                  {color === -8 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-154px 0px` }}
+                    />
+                  )}
+                  {color === -9 && (
+                    <div
+                      className={styles.imageStyle}
+                      style={{ backgroundPosition: `-240px 0px` }}
+                    />
+                  )}
                 </div>
               </div>
             )),
@@ -32,6 +107,16 @@ const Home = () => {
         </div>
         <div className={styles.whiteLineBombBottom} />
         <div className={styles.whiteLineBombRight} />
+        <div className={styles.frontBoardStyle}>
+          {frontBoard.map((row, y) =>
+            row.map((color, x) => (
+              <div className={styles.blindCellStyle} key={`${x}-${y}`}>
+                <div className={styles.whiteLineBlindUp} />
+                <div className={styles.whiteLineBlindLeft} />
+              </div>
+            )),
+          )}
+        </div>
       </div>
       <div className={styles.whiteLineBaseUp} />
       <div className={styles.whiteLineBaseLeft} />
